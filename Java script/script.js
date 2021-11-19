4c88f0d522578b6e7854c63a0b4c0f6b90e23227
AMP2168 <91816878+AMP2168@users.noreply.github.com>
2021-11-19 21:31:58 +1100

    Add files via upload
    dragging files into gh

---
+++ Java script/script.js
@@ -0,0 +1,26 @@
+
+
+function myFunctionA() {
+document.getElementById("demo13").innerHTML = "This fucker has changed";
+}
+  
+
+
+document.getElementById("demo14").innerHTML = "This is written in an external JS file";
+  
+
+let p, q, r;
+p = 10;
+q = 3;
+r = p + q;
+document.getElementById("demo15").innerHTML = "r is " + r + "";
+
+
+function myFunctionB() {
+document.getElementById("demo16").innerHTML = "Written in JS file";
+document.getElementById("demo17").innerHTML = "Also written in JS file";
+}
+
+let f;
+f = 88;
+document.getElementById("demo18").innerHTML = f; 
